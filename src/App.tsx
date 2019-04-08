8dd08d2e46bd64d4d25c84c0627f3f2d70a49f2c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2019-04-07 21:03:33 -0700

    Lazy load BurgerMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { Suspense } from "react";
+import MenuLoading from "./MenuLoading.svg";
 import "./App.css";
-import Menu from "./Menu";
+
+const Menu = React.lazy(() => import("./Menu"));
 
 const App = () => (
   <div className="App">
-    <Menu pageWrapId="App-content" outerContainerId="App" />
+    <Suspense fallback={<img src={MenuLoading} className="App-menu-loading" />}>
+      <Menu pageWrapId="App-content" outerContainerId="App" />
+    </Suspense>
     <div className="App-content">
       <header className="App-header" />
       <div className="App-Content">Nothing to see here</div>
